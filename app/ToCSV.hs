bac38dff5774626666c9253ba07cdefbea511473
Mario Blažević <blamario@protonmail.com>
2024-11-29 09:36:42 -0500

    Add proper command-line options

--- app/ToCSV.hs
+++ app/ToCSV.hs
@@ -1,16 +1,22 @@
 module Main where
 
+import Control.Applicative ((<**>), optional)
 import qualified Data.ByteString.Lazy as ByteString
 import qualified Data.Csv as CSV
 import qualified DBase (parse, csvHeader, csvRecords)
-import System.Environment (getArgs)
+import Options.Applicative (Parser, metavar)
+import qualified Options.Applicative as OptsAp
+
+optionsParser :: Parser (Maybe String)
+optionsParser =
+   optional (OptsAp.argument OptsAp.str (metavar "DBF file" <> OptsAp.action "file"))
+   <**> OptsAp.helper
 
 main :: IO ()
 main = do
-  [path] <- getArgs
-  contents <- ByteString.readFile path
+  path <- OptsAp.execParser (OptsAp.info optionsParser $ OptsAp.progDesc "Convert a DBF file to CSV")
+  contents <- maybe ByteString.getContents ByteString.readFile path
   let dbf = case DBase.parse contents of
               Left err -> error err
               Right parsed -> parsed
   ByteString.putStr (CSV.encode $ DBase.csvHeader dbf : DBase.csvRecords dbf)
-
